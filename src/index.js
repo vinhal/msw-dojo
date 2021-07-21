6299d06d4fa9287befa727d523abf59c22735360
Lucas Vinhal <lucvinhal@gmail.com>
2021-07-21 09:54:59 -0300

    Updating worker load condition

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from "react-dom";
 import App from "./App";
 import "./index.css";
 
-if (process.env.REACT_APP_MOCK) {
+if (process.env.NODE_ENV === 'development' && process.env.REACT_APP_MOCK) {
   const { worker } = require("./mocks/browser");
   worker.start({ onUnhandledRequest: 'bypass' });
 }
